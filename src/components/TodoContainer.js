b4f14ab12347d7d549ec52dc82df118b3e6a2aba
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-09-21 17:04:41 +0500

    Add delete functionality

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -24,16 +24,29 @@ class TodoContainer extends React.Component {
   };
 
   handleChange = (id) => {
-    this.setState({
-      todos: this.state.todos.map((todo) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) => {
         if (todo.id === id) {
-          todo.completed = !todo.completed;
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
         }
         return todo;
       }),
+    }));
+  };
+
+  delTodo = (id) => {
+    this.setState({
+      todos: [
+        ...this.state.todos.filter((todo) => {
+          return todo.id !== id;
+        }),
+      ],
     });
   };
-  
+
   render() {
     return (
       <>
@@ -41,6 +54,7 @@ class TodoContainer extends React.Component {
         <TodosList
           todos={this.state.todos}
           handleChangeProps={this.handleChange}
+          deleteTodoProps={this.delTodo}
         />
       </>
     );
